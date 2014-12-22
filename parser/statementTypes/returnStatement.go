ff3dc1f03e99154e7838f077b62574b39b0ac8fc
Andrew Poydence <apoydence@gmail.com>
2014-12-21 23:17:52 -0700

    Return statement figures out the correct return type

--- parser/statementTypes/returnStatement.go
+++ parser/statementTypes/returnStatement.go
@@ -11,6 +11,11 @@ type ReturnStatement struct {
 	outputQueue []string
 }
 
+type blockSpec struct {
+	block     string
+	valueType TypeName
+}
+
 func NewReturnStatementParser() StatementParser {
 	return ReturnStatement{}
 }
@@ -37,14 +42,16 @@ func (ds *ReturnStatement) GenerateGo(fm FunctionMap) (string, error) {
 		return "", err
 	}
 
-	return wrapCode(toInfix(ops, 0))
+	bs := toBlockSpec(ops)
+
+	return wrapCode(toInfix(bs, 0))
 }
 
-func wrapCode(code string, err error) (string, error) {
+func wrapCode(code string, returnType TypeName, err error) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	return fmt.Sprintf("func() int {\n\t%s\n}", code), nil
+	return fmt.Sprintf("func() %s {\n\t%s\n}", returnType, code), nil
 }
 
 // This uses the shunting-yard algorithm
@@ -83,20 +90,61 @@ func toRpn(line string, outputQueue []string, opStack []string) ([]string, error
 	return nil, errors.New("Unknown token: " + token)
 }
 
-func toInfix(opQueue []string, index int) (string, error) {
+func toInfix(opQueue []*blockSpec, index int) (string, TypeName, error) {
 	if len(opQueue) <= index {
-		return opQueue[0], nil
-	} else if parser.IsOperator(opQueue[index]) {
-		combined := fmt.Sprintf("(%s%s%s)", addTypeToNumber(opQueue[index-2]), opQueue[index], addTypeToNumber(opQueue[index-1]))
-		return toInfix(append(opQueue[:index-2], combine(combined, opQueue[index+1:])...), index-2)
+		return opQueue[0].block, opQueue[0].valueType, nil
+	} else if parser.IsOperator(opQueue[index].block) {
+		left, _ := addTypeToNumber(opQueue[index-2].block)
+		right, _ := addTypeToNumber(opQueue[index-1].block)
+		combined := fmt.Sprintf("(%s%s%s)", left, opQueue[index].block, right)
+		value, err := getValueType(opQueue[index-2 : 3])
+		if err != nil {
+			return "", "", err
+		}
+		//op := newBlockSpec(combined, value)
+		op := newBlockSpec(combined, value)
+		return toInfix(append(opQueue[:index-2], combine(op, opQueue[index+1:])...), index-2)
 	} else {
 		return toInfix(opQueue, index+1)
 	}
 }
 
-func addTypeToNumber(token string) string {
-	if !parser.IsNumber(token) || len(token) <= 1 {
-		return token
+func getValueType(ops []*blockSpec) (TypeName, error) {
+	left := ops[0].valueType
+	right := ops[1].valueType
+	if ops[0].valueType == "" {
+		_, left = addTypeToNumber(ops[0].block)
+	}
+	if ops[1].valueType == "" {
+		_, right = addTypeToNumber(ops[1].block)
+	}
+	if left != right {
+		return "", errors.New(fmt.Sprintf("Illegal to %s%s%s", left, right, ops[2]))
+	}
+
+	return left, nil
+}
+
+func newBlockSpec(op string, valueType TypeName) *blockSpec {
+	return &blockSpec{
+		block:     op,
+		valueType: valueType,
+	}
+}
+
+func toBlockSpec(opQueue []string) []*blockSpec {
+	result := make([]*blockSpec, 0)
+	for _, o := range opQueue {
+		result = append(result, newBlockSpec(o, ""))
+	}
+	return result
+}
+
+func addTypeToNumber(token string) (string, TypeName) {
+	if !parser.IsNumber(token) {
+		return token, ""
+	} else if len(token) <= 1 {
+		return token, "int32"
 	}
 
 	suffix1 := token[len(token)-1:]
@@ -110,30 +158,30 @@ func addTypeToNumber(token string) string {
 	}
 
 	if suffix2 == "ub" {
-		return fmt.Sprintf("uint8(%s)", token2)
+		return fmt.Sprintf("uint8(%s)", token2), "uint8"
 	} else if suffix1 == "b" {
-		return fmt.Sprintf("int8(%s)", token1)
+		return fmt.Sprintf("int8(%s)", token1), "int8"
 	} else if suffix2 == "uh" {
-		return fmt.Sprintf("uint16(%s)", token2)
+		return fmt.Sprintf("uint16(%s)", token2), "uint16"
 	} else if suffix1 == "h" {
-		return fmt.Sprintf("int16(%s)", token1)
+		return fmt.Sprintf("int16(%s)", token1), "int16"
 	} else if suffix2 == "ui" {
-		return fmt.Sprintf("uint32(%s)", token2)
+		return fmt.Sprintf("uint32(%s)", token2), "uint32"
 	} else if suffix2 == "ul" {
-		return fmt.Sprintf("uint64(%s)", token2)
+		return fmt.Sprintf("uint64(%s)", token2), "uint64"
 	} else if suffix1 == "l" {
-		return fmt.Sprintf("int64(%s)", token1)
+		return fmt.Sprintf("int64(%s)", token1), "int64"
 	} else if suffix1 == "f" {
-		return fmt.Sprintf("float32(%s)", token1)
+		return fmt.Sprintf("float32(%s)", token1), "float32"
 	} else if len(suffix2) > 0 && suffix2[0] == '.' {
-		return fmt.Sprintf("float64(%s)", token)
+		return fmt.Sprintf("float64(%s)", token), "float64"
 	} else {
-		return token
+		return token, "int32"
 	}
 }
 
-func combine(x string, y []string) []string {
-	return append([]string{x}, y...)
+func combine(x *blockSpec, y []*blockSpec) []*blockSpec {
+	return append([]*blockSpec{x}, y...)
 }
 
 func opPrec(op string) int {
--- parser/statementTypes/returnStatement_test.go
+++ parser/statementTypes/returnStatement_test.go
@@ -24,19 +24,21 @@ var _ = Describe("ReturnStatement", func() {
 		})
 	})
 	Context("GenerateGo", func() {
-		It("Should generate the proper Go code with no functions", func() {
-			code := "( 1 + 11 ) - 10 / 2"
-			r := statementParser.Parse(code, nil, factory)
-			genGo, err := r.GenerateGo(nil)
-			Expect(err).To(BeNil())
-			Expect(genGo).To(Equal("func() int {\n\t((1+11)-(10/2))\n}"))
-		})
-		It("Should generate the proper Go code with proper numeric types", func() {
-			code := "( 1b + 11b ) - 10b / 2b"
-			r := statementParser.Parse(code, nil, factory)
-			genGo, err := r.GenerateGo(nil)
-			Expect(err).To(BeNil())
-			Expect(genGo).To(Equal("func() int {\n\t((int8(1)+int8(11))-(int8(10)/int8(2)))\n}"))
+		Context("No functions", func() {
+			It("Should generate the proper Go code", func() {
+				code := "( 1 + 11 ) - 10 / 2"
+				r := statementParser.Parse(code, nil, factory)
+				genGo, err := r.GenerateGo(nil)
+				Expect(err).To(BeNil())
+				Expect(genGo).To(Equal("func() int32 {\n\t((1+11)-(10/2))\n}"))
+			})
+			It("Should generate the proper Go code with proper numeric types", func() {
+				code := "( 1b + 11b ) - 10b / 2b"
+				r := statementParser.Parse(code, nil, factory)
+				genGo, err := r.GenerateGo(nil)
+				Expect(err).To(BeNil())
+				Expect(genGo).To(Equal("func() int8 {\n\t((int8(1)+int8(11))-(int8(10)/int8(2)))\n}"))
+			})
 		})
 	})
 })
