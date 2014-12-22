02e64e6726c08948a0144b353ca82a75174cdaf3
Andrew Poydence <apoydence@gmail.com>
2014-12-22 06:25:05 -0700

    Implements FunctionDeclaration

--- parser/statementTypes/functionMap.go
+++ parser/statementTypes/functionMap.go
@@ -6,24 +6,24 @@ import (
 )
 
 type funcMap struct {
-	fm    map[string]FunctionDeclaration
+	fm    map[string]*FunctionDeclaration
 	count int
 }
 
 func NewFunctionMap() FunctionMap {
 	return &funcMap{
-		fm: make(map[string]FunctionDeclaration),
+		fm: make(map[string]*FunctionDeclaration),
 	}
 }
 
-func (fm *funcMap) GetFunction(name string) FunctionDeclaration {
+func (fm *funcMap) GetFunction(name string) *FunctionDeclaration {
 	if d, ok := fm.fm[name]; ok {
 		return d
 	}
 	return nil
 }
 
-func (fm *funcMap) AddFunction(name string, f FunctionDeclaration) (string, error) {
+func (fm *funcMap) AddFunction(name string, f *FunctionDeclaration) (string, error) {
 	if _, ok := fm.fm[name]; ok {
 		return "", errors.New("The function name'" + name + "' is already allocated.")
 	}
