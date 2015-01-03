b43ba4ab7faaf32c6566de2acff7480077ffe095
Andrew Poydence <apoydence@gmail.com>
2015-01-03 08:37:12 -0700

    functionStatement tests multiple inner statements

--- parser/statementTypes/functionStatement.go
+++ parser/statementTypes/functionStatement.go
@@ -14,20 +14,20 @@ func init() {
 }
 
 type FunctionStatement struct {
-	FuncName       string
-	TypeDef        expressionParsing.TypeDefinition
-	InnerStatement Statement
+	FuncName        string
+	TypeDef         expressionParsing.TypeDefinition
+	InnerStatements []Statement
 }
 
 func NewFunctionStatementParser() StatementParser {
 	return FunctionStatement{}
 }
 
-func newFunctionStatement(name string, typeDef *expressionParsing.FuncTypeDefinition, inner Statement) Statement {
+func newFunctionStatement(name string, typeDef *expressionParsing.FuncTypeDefinition, inner []Statement) Statement {
 	return &FunctionStatement{
-		FuncName:       name,
-		TypeDef:        typeDef,
-		InnerStatement: inner,
+		FuncName:        name,
+		TypeDef:         typeDef,
+		InnerStatements: inner,
 	}
 }
 
@@ -42,10 +42,29 @@ func (fs FunctionStatement) Parse(block string, nextBlockScanner *parser.ScanPee
 	if err != nil {
 		return nil
 	}
-	scanner := parser.NewScanPeekerStr(parser.FromLines(lines[1:]))
-	innerStatement := factory.Read(scanner)
+	innerStatements := fetchInnerStatements(lines[1:], factory)
 
-	return newFunctionStatement(name, typeDef, innerStatement)
+	// CHECK TO SEE IF THE LAST STATEMENT IS A DECLARATION
+
+	return newFunctionStatement(name, typeDef, innerStatements)
+}
+
+func fetchInnerStatements(lines []string, factory *StatementFactory) []Statement {
+	scanner := parser.NewScanPeekerStr(parser.FromLines(lines))
+	statements := make([]Statement, 0)
+	next := func() Statement {
+		return factory.Read(scanner)
+	}
+
+	return subFetchInnerStatements(next, statements)
+}
+
+func subFetchInnerStatements(next func() Statement, statements []Statement) []Statement {
+	if s := next(); s != nil {
+		return subFetchInnerStatements(next, append(statements, s))
+	}
+
+	return statements
 }
 
 func fetchParts(code string) (string, string, bool) {
@@ -64,8 +83,24 @@ func fetchParts(code string) (string, string, bool) {
 
 func (fs *FunctionStatement) GenerateGo(fm expressionParsing.FunctionMap) (string, expressionParsing.TypeDefinition, error) {
 	setupFuncMap(fm, fs.TypeDef.(*expressionParsing.FuncTypeDefinition))
-	inner, _, _ := fs.InnerStatement.GenerateGo(fm.NextScopeLayer())
+	inner, err := generateInnerGo(fm, fs.InnerStatements)
+	if err != nil {
+		return "", nil, err
+	}
 	return fmt.Sprintf("func %s %s{\n\t%s\n}", fs.FuncName, generateTypeDef(true, fs.TypeDef), generateInnerFunc(fs.TypeDef, 1, inner)), fs.TypeDef, nil
+}
+
+func generateInnerGo(fm expressionParsing.FunctionMap, statements []Statement) ([]string, error) {
+	code := make([]string, 0)
+	for _, s := range statements {
+		c, _, err := s.GenerateGo(fm)
+		if err != nil {
+			return nil, err
+		}
+		code = append(code, c)
+	}
+
+	return code, nil
 }
 
 func setupFuncMap(fm expressionParsing.FunctionMap, typeDef *expressionParsing.FuncTypeDefinition) {
@@ -76,16 +111,20 @@ func setupFuncMap(fm expressionParsing.FunctionMap, typeDef *expressionParsing.F
 	fm.AddFunction(typeDef.ArgumentName, newFt)
 }
 
-func generateInnerFunc(typeDef expressionParsing.TypeDefinition, tabCount int, innerStatement string) string {
-	if !typeDef.ReturnType().IsFunc() {
-		return fmt.Sprintf("return %s", innerStatement)
-	}
+func generateInnerFunc(typeDef expressionParsing.TypeDefinition, tabCount int, innerStatements []string) string {
 	tabs := ""
 	for i := 0; i <= tabCount; i++ {
 		tabs += "\t"
 	}
 	tabs2 := string(tabs[:len(tabs)-1])
-	return fmt.Sprintf("return %s {\n%s%s\n%s}", generateTypeDef(false, typeDef.ReturnType()), tabs, generateInnerFunc(typeDef.ReturnType(), tabCount+1, innerStatement), tabs2)
+
+	if !typeDef.ReturnType().IsFunc() {
+		lenInner := len(innerStatements) - 1
+		innerCode := parser.FromLines(innerStatements[:lenInner])
+		return fmt.Sprintf("%s\n%sreturn %s", innerCode, tabs2, innerStatements[lenInner])
+	}
+
+	return fmt.Sprintf("return %s {\n%s%s\n%s}", generateTypeDef(false, typeDef.ReturnType()), tabs, generateInnerFunc(typeDef.ReturnType(), tabCount+1, innerStatements), tabs2)
 }
 
 func generateTypeDef(first bool, typeDef expressionParsing.TypeDefinition) string {
