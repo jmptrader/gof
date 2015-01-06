da18f4deb963e541681aaf2ec58cb02e27f8ab2c
Andrew Poydence <apoydence@gmail.com>
2015-01-06 06:46:03 -0700

    Adds SyntaxError and expressionParsing uses it.

--- parser/expressionParsing/expressionParser.go
+++ parser/expressionParsing/expressionParser.go
@@ -1,16 +1,18 @@
 package expressionParsing
 
+import "github.com/apoydence/GoF/parser"
+
 type FunctionMap interface {
 	GetFunction(name string) TypeDefinition
 	AddFunction(name string, f TypeDefinition) (string, error)
 	NextScopeLayer() FunctionMap
 }
 
-func ToRpn(line string, fm FunctionMap) ([]string, error) {
+func ToRpn(line string, fm FunctionMap) ([]string, parser.SyntaxError) {
 	return toRpn(line, []rpnValue{}, []rpnValue{}, fm)
 }
 
-func ToGoExpression(line string, fm FunctionMap) (string, TypeDefinition, error) {
+func ToGoExpression(line string, fm FunctionMap) (string, TypeDefinition, parser.SyntaxError) {
 	rpn, err := ToRpn(line, fm)
 	if err != nil {
 		return "", nil, err
