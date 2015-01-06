d8cabd463d3e9eafaf397486edeb2661c2157940
Andrew Poydence <apoydence@gmail.com>
2015-01-06 15:23:26 -0700

    Adds the gof generate tool

---
+++ gofTool/tests/singleFunc_test.go
@@ -0,0 +1,20 @@
+package tests_test
+
+import (
+	. "github.com/apoydence/GoF/gofTool/tests"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("SingleFunc", func() {
+	Context("AddTogether", func() {
+		It("Should add the three numbers together", func() {
+			Expect(AddTogether(1)(2)(3)).To(BeEquivalentTo(6))
+			addTo5 := AddTogether(5)
+			Expect(addTo5(1)(2)).To(BeEquivalentTo(8))
+			addTo5And6 := addTo5(6)
+			Expect(addTo5And6(1)).To(BeEquivalentTo(12))
+		})
+	})
+})
---
+++ gofTool/tests/twoFunc_test.go
@@ -0,0 +1,16 @@
+package tests_test
+
+import (
+	. "github.com/apoydence/GoF/gofTool/tests"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("TwoFunc", func() {
+	Context("Action", func() {
+		It("Should multiply the first two values and then add the third", func() {
+			Expect(Action(2)(3)(4)).To(BeEquivalentTo(10))
+		})
+	})
+})
