c1e182290f7ee6e535cd242fbdd258ead4e0f299
Andrew Poydence <apoydence@gmail.com>
2015-01-11 16:55:53 -0700

    Updates endToEnd

--- tool/tests/twoFunc_test.go
+++ tool/tests/twoFunc_test.go
@@ -10,7 +10,8 @@ import (
 var _ = Describe("TwoFunc", func() {
 	Context("Action", func() {
 		It("Should multiply the first two values and then add the third", func() {
-			Expect(Action(2)(3)(4)).To(BeEquivalentTo(10))
+			Expect(Action(2)(3)(4)).To(BeEquivalentTo(15))
+			Expect(Action(3)(4)(5)).To(BeEquivalentTo(24))
 		})
 	})
 })
